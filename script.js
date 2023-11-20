3f52f223a0bc1a46a8fa3193011b3adb34156c3a
brimwell <142336400+brimwell@users.noreply.github.com>
2023-11-20 11:01:44 -0700

    Change constructor js to class.

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ const main = document.querySelector('.library');
 
 //Constructor for a Book Object
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        if (read === false) {
-            return `${title} by ${author}, ${pages} pages, not read yet`;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
+        if (this.read === false) {
+            return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
         } else {
-            return `${title} by ${author}, ${pages} pages, already read`;
+            return `${this.title} by ${this.author}, ${this.pages} pages, already read`;
         }
     }
 }
